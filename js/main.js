d9c694e26205fbaec7cf38dcf7d185e5edb5b5b3
marq <makolodziej86@gmail.com>
2022-01-15 20:12:27 +0100

    mobile nav problem solved

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer_year');
+const allNavItems = document.querySelectorAll('.nav__link');
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
     navMobile.classList.toggle('nav-mobile--active');
+
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () =>{
+          navMobile.classList.remove('nav-mobile--active');
+          navBtn.classList.remove('is-active');
+        })  
+      })
 }
 
 navBtn.addEventListener('click', handleNav);
